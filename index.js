2fea8db082968a28473f8e79a860eb40e5b38904
Sarthak <mehtasarthak2@gmail.com>
2024-08-09 02:11:48 +0530

    CORS errrrrr

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ const app = express();
 // }));
 
 app.use((req, res, next) => {
-    // res.header("Access-Control-Allow-Origin", "https://www.sharpmoney.co.in"); //use this for prod
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173" ) //use this for dev
+    res.header("Access-Control-Allow-Origin", "https://www.sharpmoney.co.in"); //use this for prod
+    // res.header("Access-Control-Allow-Origin", "http://localhost:5173" ) //use this for dev
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
